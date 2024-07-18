f2ee4e5b2dae97f78b6735be4b0105afd07316b7
jhonatanlituma1 <jslituma@pichincha.com>
2024-07-17 21:08:05 -0500

    fix tabs names

--- nots/app/(tabs)/_layout.tsx
+++ nots/app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -20,26 +20,37 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
         headerShadowVisible: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Menú',
+          title: "Inicio",
           tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].primaryColor,
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].primaryColor,
+        }}
+      />
+      <Tabs.Screen
+        name="schedule"
+        options={{
+          title: "Horario",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="calendar" color={color} />
+          ),
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].primaryColor,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Perfil',
+          title: "Perfil",
           tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].primaryColor,
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].primaryColor,
         }}
       />
     </Tabs>
